60c8e2986d450731668b4aee1223ebf339867e7c
Wagner Caetano <wcaetano@ciandt.com>
2024-11-26 00:04:13 -0300

    fixing redirect

--- src/app/pages/auth/auth.component.ts
+++ src/app/pages/auth/auth.component.ts
@@ -1,23 +1,11 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { AuthService } from '@core/services/auth.service';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
 })
-export class AuthComponent implements OnInit {
+export class AuthComponent {
   showLogin = true;
-
-  constructor(private readonly activatedRoute: ActivatedRoute, private readonly authService: AuthService) {}
-
-  ngOnInit(): void {
-    this.activatedRoute.url.subscribe(async (url) => {
-      this.showLogin = url[0].path === 'login';
-      let data = await this.authService.getUserData();
-      console.log(data);
-    });
-  }
 
   toggleForm() {
     this.showLogin = !this.showLogin;
