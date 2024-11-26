42778354a1a0a8894b65c841a9f6cab69554240b
Wagner Caetano <wcaetano@ciandt.com>
2024-11-25 23:29:41 -0300

    update do routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
     path: 'login',
     loadChildren: () =>
